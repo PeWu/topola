f68ae89fa10f03b0ed6585a2a0ff1810fa66618c
Tomasz Więch <tewu.dev@gmail.com>
2019-06-07 18:55:11 +0200

    Refactor

--- src/kinship-chart.ts
+++ src/kinship-chart.ts
@@ -3,19 +3,18 @@ import { Chart, ChartInfo, ChartOptions, DataProvider, Indi, Fam } from './api';
 import { TreeNode } from './kinship/api';
 import { HierarchyCreator, EntryId } from './kinship/hierarchy-creator';
 import { KinshipChartRenderer } from './kinship/renderer';
+import { deepFreeze } from './utils'
 
 
 export class KinshipChart implements Chart {
-  readonly data: DataProvider<Indi, Fam>;
   readonly renderer: KinshipChartRenderer;
 
   constructor(readonly options: ChartOptions) {
-    this.data = options.data;
     this.renderer = new KinshipChartRenderer(this.options);
   }
 
   render(): ChartInfo {
-    const hierarchyCreator = new HierarchyCreator(this.data, new EntryId(this.options.startIndi, this.options.startFam));
+    const hierarchyCreator = new HierarchyCreator(this.options.data, new EntryId(this.options.startIndi, this.options.startFam));
     const [upNodes, downNodes] = this.renderer.layOut(hierarchyCreator.getUpRoot(), hierarchyCreator.getDownRoot());
 
     upNodes.concat(downNodes).forEach(node => {
@@ -26,29 +25,45 @@ export class KinshipChart implements Chart {
   }
 
   private setChildNodesGenerationNumber(node: d3.HierarchyNode<TreeNode>) {
-    const [indiParentsNodes, indiSiblingsNodes, spouseParentsNodes,
-      spouseSiblingsNodes, childrenNodes] = this.getChildNodesByType(node);
+    const childNodes = this.getChildNodesByType(node);
     const setGenerationNumber = (childNode: d3.HierarchyNode<TreeNode>, value: number) =>
       childNode.data.generation = node.data.generation + value;
 
-    indiParentsNodes.forEach(n =>    setGenerationNumber(n, -1));
-    indiSiblingsNodes.forEach(n =>   setGenerationNumber(n,  0));
-    spouseParentsNodes.forEach(n =>  setGenerationNumber(n, -1));
-    spouseSiblingsNodes.forEach(n => setGenerationNumber(n,  0));
-    childrenNodes.forEach(n =>       setGenerationNumber(n,  1));
+    childNodes.indiParents.forEach(n =>    setGenerationNumber(n, -1));
+    childNodes.indiSiblings.forEach(n =>   setGenerationNumber(n,  0));
+    childNodes.spouseParents.forEach(n =>  setGenerationNumber(n, -1));
+    childNodes.spouseSiblings.forEach(n => setGenerationNumber(n,  0));
+    childNodes.children.forEach(n =>       setGenerationNumber(n,  1));
   }
 
-  private getChildNodesByType<N extends d3.HierarchyNode<TreeNode>>(node: N): [N[], N[], N[], N[], N[]] {
-    if (!node || !node.children) return [[], [], [], [], []];
-    const childNodesById = new Map(node.children.map(n => [n.data.id, n] as [string, N]));
+  private getChildNodesByType(node: d3.HierarchyNode<TreeNode>): HierarchyTreeNodes {
+    if (!node || !node.children) return EMPTY_HIERARCHY_TREE_NODES;
+    // Maps id to node object for all children of the input node
+    const childNodesById = new Map(node.children.map(n => [n.data.id, n] as [string, d3.HierarchyNode<TreeNode>]));
     const nodeToHNode = (n: TreeNode) => n ? childNodesById.get(n.id) : null;
     const childNodes = node.data.childNodes;
-    return [
-      childNodes.indiParents.map(nodeToHNode),
-      childNodes.indiSiblings.map(nodeToHNode),
-      childNodes.spouseParents.map(nodeToHNode),
-      childNodes.spouseSiblings.map(nodeToHNode),
-      childNodes.children.map(nodeToHNode),
-    ];
+    return {
+      indiParents: childNodes.indiParents.map(nodeToHNode),
+      indiSiblings: childNodes.indiSiblings.map(nodeToHNode),
+      spouseParents: childNodes.spouseParents.map(nodeToHNode),
+      spouseSiblings: childNodes.spouseSiblings.map(nodeToHNode),
+      children: childNodes.children.map(nodeToHNode),
+    };
   }
 }
+
+
+interface HierarchyTreeNodes {
+  indiParents: Array<d3.HierarchyNode<TreeNode>>;
+  indiSiblings: Array<d3.HierarchyNode<TreeNode>>;
+  spouseParents: Array<d3.HierarchyNode<TreeNode>>;
+  spouseSiblings: Array<d3.HierarchyNode<TreeNode>>;
+  children: Array<d3.HierarchyNode<TreeNode>>;
+}
+const EMPTY_HIERARCHY_TREE_NODES: HierarchyTreeNodes = deepFreeze({
+  indiParents: [],
+  indiSiblings: [],
+  spouseParents: [],
+  spouseSiblings: [],
+  children: [],
+});
