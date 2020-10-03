c2e01426facc6b8887bdc8810f52fbe948bca449
Przemek Wiech <pwiech@gmail.com>
2020-10-03 22:45:26 +0200

    Updates

--- bundle.js
+++ bundle.js
@@ -7721,22 +7721,38 @@ exports.JsonDataProvider = JsonDataProvider;
 },{}],20:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var MONTHS_EN = new Map([
-    [1, 'Jan'],
-    [2, 'Feb'],
-    [3, 'Mar'],
-    [4, 'Apr'],
-    [5, 'May'],
-    [6, 'Jun'],
-    [7, 'Jul'],
-    [8, 'Aug'],
-    [9, 'Sep'],
-    [10, 'Oct'],
-    [11, 'Nov'],
-    [12, 'Dec'],
-]);
 /** Translations of the GEDCOM date qualifiers. */
 var QUALIFIERS_I18N = new Map([
+    [
+        'de',
+        new Map([
+            ['cal', 'errech.'],
+            ['abt', 'etwa'],
+            ['est', 'geschät.'],
+            ['before', 'vor'],
+            ['after', 'nach'],
+        ]),
+    ],
+    [
+        'fr',
+        new Map([
+            ['cal', 'calc.'],
+            ['abt', 'vers'],
+            ['est', 'est.'],
+            ['before', 'avant'],
+            ['after', 'après'],
+        ]),
+    ],
+    [
+        'it',
+        new Map([
+            ['cal', 'calc.'],
+            ['abt', 'circa il'],
+            ['est', 'stim.'],
+            ['before', 'prima del'],
+            ['after', 'dopo del'],
+        ]),
+    ],
     [
         'pl',
         new Map([
@@ -7752,43 +7768,44 @@ var QUALIFIERS_I18N = new Map([
         new Map([
             ['cal', 'выч.'],
             ['abt', 'ок.'],
-            ['est', 'прибл.'],
+            ['est', 'оцен.'],
             ['before', 'до'],
             ['after', 'после'],
         ]),
     ],
 ]);
-var shortMonthCache = new Map();
-function getShortMonth(month, locale) {
-    if (!Intl || !Intl.DateTimeFormat) {
-        return MONTHS_EN.get(month);
-    }
-    var cacheKey = month + "|" + (locale || '');
-    if (shortMonthCache.has(cacheKey)) {
-        return shortMonthCache.get(cacheKey);
-    }
-    var result = new Intl.DateTimeFormat(locale, { month: 'short' }).format(new Date(2000, month - 1));
-    shortMonthCache.set(cacheKey, result);
-    return result;
-}
 function getQualifier(qualifier, locale) {
     var language = locale && locale.split(/[-_]/)[0];
     var languageMap = language && QUALIFIERS_I18N.get(language);
     return languageMap ? languageMap.get(qualifier) : qualifier;
 }
+/**
+ * Formats the date consisting of day, month and year.
+ * All parts of the date are optional.
+ */
+function formatDateOnly(day, month, year, locale) {
+    if (!day && !month && !year) {
+        return '';
+    }
+    var format = {
+        day: day ? 'numeric' : undefined,
+        month: month ? 'short' : undefined,
+        year: year ? 'numeric' : undefined,
+    };
+    return new Intl.DateTimeFormat(locale, format).format(new Date(year !== null && year !== void 0 ? year : 2000, month ? month - 1 : 1, day !== null && day !== void 0 ? day : 1));
+}
 /** Simple date formatter. */
 function formatDate(date, locale) {
     return [
         date.qualifier && getQualifier(date.qualifier, locale),
-        date.day,
-        date.month && getShortMonth(date.month, locale),
-        date.year,
+        formatDateOnly(date.day, date.month, date.year, locale),
         date.text,
     ].join(' ');
 }
 exports.formatDate = formatDate;
 /** Formats a DateOrRange object. */
 function formatDateOrRange(dateOrRange, locale) {
+    locale = 'ru';
     if (dateOrRange.date) {
         return formatDate(dateOrRange.date, locale);
     }
@@ -8279,7 +8296,7 @@ var DetailedRenderer = /** @class */ (function (_super) {
         var id = enter
             .append('text')
             .attr('class', 'id')
-            .text(function (data) { return getIndi(data).showId() ? data.indi.id : ''; })
+            .text(function (data) { return (getIndi(data).showId() ? data.indi.id : ''); })
             .merge(update.select('text.id'));
         this.transition(id).attr('transform', function (data) { return "translate(9, " + (data.indi.height - 5) + ")"; });
         // Render sex.
@@ -8631,7 +8648,10 @@ function parseDate(parts) {
 }
 /** Parses a GEDCOM date or date range. */
 function getDate(gedcomDate) {
-    var parts = gedcomDate.split(' ');
+    var parts = gedcomDate
+        .replace(/@.*@/, '')
+        .trim()
+        .split(' ');
     var firstPart = parts[0].toLowerCase();
     if (firstPart.startsWith('bet')) {
         var i = parts.findIndex(function (x) { return x.toLowerCase() === 'and'; });
