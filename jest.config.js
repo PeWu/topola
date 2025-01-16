c44274e89ef292a0af9dd5c702c58596a1d76ddf
Przemek Więch <pwiech@gmail.com>
2025-01-16 16:21:55 +0100

    Add `d3-color` to `jest.config.js`

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "d3-array": "<rootDir>/node_modules/d3-array/dist/d3-array.js",
+    "d3-color": "<rootDir>/node_modules/d3-color/dist/d3-color.js",
     "d3-hierarchy": "<rootDir>/node_modules/d3-hierarchy/dist/d3-hierarchy.js",
     "d3-selection": "<rootDir>/node_modules/d3-selection/dist/d3-selection.js",
     "d3-transition": "<rootDir>/node_modules/d3-transition/dist/d3-transition.js",
